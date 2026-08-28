aaa05529f0e4c6298129ee6c3f9a0cfa86a96bb9
Yang9997 <1974442074@qq.com>
2026-08-29 00:26:40 +0800

    2026 0829 1

--- Class9/2froshims/app.py
+++ Class9/2froshims/app.py
@@ -1,37 +1,33 @@
 from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
-REGISTRANTS = {}
+
 SPORTS = [
-    "Basketball",
-    "Soccer",
-    "Ultimate Frisbee"
+    "soccer",
+    "basketball",
+    "swimming",
+    "volleyball"
 ]
 
-@app.route("/", methods=["GET", "POST"])
+REGISTRANTS = {}
+
+@app.route("/")
 def index():
-    return  render_template("index.html", sports=SPORTS)
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
-    # 校验姓名
     name = request.form.get("name")
     if not name:
-        return render_template("error.html", message="There is no name in form.")
-
-    # 校验运动项目
+        return render_template("error.html", message="No Name")
     sport = request.form.get("sport")
-    if not sport:
-       return render_template("error.html", message="There is no sport in form.")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Invalid sport.")
+    if not sport or sport not in SPORTS:
+        return render_template("error.html", message="No Sport or Invalid Sport")
 
-    # 保存注册者信息
-    # 这是个字典
     REGISTRANTS[name] = sport
-
-    # 注册成功，正在跳转
     return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
     return render_template("registrants.html", registrants=REGISTRANTS)
+
